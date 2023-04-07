2f077d65dc1323ec37e01114324eed1973c561fe
Денис <den.wtrui2017@gmail.com>
2023-04-07 09:37:56 +0200

    no message

---
+++ HR_Department/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HR_Department
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
